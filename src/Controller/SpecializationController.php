b45c840ab76a13fe1b181207454165dd88f81e42
Orden14 <t.lesrel@gmail.com>
2024-04-02 23:57:40 +0200

    added new spec modal

--- src/Controller/SpecializationController.php
+++ src/Controller/SpecializationController.php
@@ -25,21 +25,36 @@ class SpecializationController extends AbstractController
         private readonly SpecializationRepository $specializationRepository
     ) {}
 
-    #[Route('/', name: 'index', methods: ['GET'])]
-    final public function index(): Response
+    #[Route('/', name: 'index', methods: ['GET', 'POST'])]
+    final public function index(Request $request): Response
     {
+        $specialization = new Specialization();
+        $form = $this->createForm(SpecializationType::class, $specialization, [
+            'action' => $this->generateUrl('specialization_new'),
+            'method' => 'POST',
+        ]);
+        $form->handleRequest($request);
+
         return $this->render('specialization/index.html.twig', [
-            'specializations' => $this->specializationRepository->findAllOrderedByJob(),
+            'form' => $form->createView(),
+            'specializations' => $this->specializationRepository->findAllOrderedByJob()
         ]);
     }
 
-    #[Route('/sort/{id}', name: 'show_by_job', methods: ['GET'])]
-    final public function showByJob(Job $job): Response
+    #[Route('/sort/{id}', name: 'show_by_job', methods: ['GET', 'POST'])]
+    final public function showByJob(Job $job, Request $request): Response
     {
         $specializations = $this->specializationRepository->findBy(['job' => $job]);
+        $specialization = new Specialization();
+        $form = $this->createForm(SpecializationType::class, $specialization, [
+            'action' => $this->generateUrl('specialization_new'),
+            'method' => 'POST',
+        ]);
+        $form->handleRequest($request);
 
         return $this->render('specialization/index.html.twig', [
             'fromJobView' => true,
+            'form' => $form->createView(),
             'specializations' => $specializations,
         ]);
     }
@@ -51,16 +66,23 @@ class SpecializationController extends AbstractController
         $form = $this->createForm(SpecializationType::class, $specialization);
         $form->handleRequest($request);
 
+
         if ($form->isSubmitted() && $form->isValid()) {
+            $icon = $form->get('icon')->getData();
+            if ($icon) {
+                $newFileName = $this->fileManager->uploadFile($icon, $this->getParameter('icon_directory'));
+                $specialization->setIcon($newFileName);
+            }
+
             $this->entityManager->persist($specialization);
             $this->entityManager->flush();
 
             return $this->redirectToRoute('specialization_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('specialization/new.html.twig', [
-            'specialization' => $specialization,
+        return $this->render('specialization/index.html.twig', [
             'form' => $form,
+            'specialization' => $specialization,
         ]);
     }
 
@@ -72,7 +94,6 @@ class SpecializationController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $icon = $form->get('icon')->getData();
-
             if ($icon) {
                 if ($specialization->getIcon()) {
                     $this->fileManager->removeFile($specialization->getIcon(), $this->getParameter('icon_directory'));
@@ -102,6 +123,10 @@ class SpecializationController extends AbstractController
     final public function delete(Request $request, Specialization $specialization): Response
     {
         if ($this->isCsrfTokenValid('delete'.$specialization->getId(), $request->getPayload()->get('_token'))) {
+            if ($specialization->getIcon()) {
+                $this->fileManager->removeFile($specialization->getIcon(), $this->getParameter('icon_directory'));
+            }
+
             $this->entityManager->remove($specialization);
             $this->entityManager->flush();
         }
--- src/Form/SpecializationType.php
+++ src/Form/SpecializationType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Job;
 use App\Entity\Specialization;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,7 +19,7 @@ class SpecializationType extends AbstractType
             ->add('name')
             ->add('job', EntityType::class, [
                 'class' => Job::class,
-                'choice_label' => 'name',
+                'choice_label' => 'name'
             ])
             ->add('icon', FileType::class, [
                 'mapped' => false,
