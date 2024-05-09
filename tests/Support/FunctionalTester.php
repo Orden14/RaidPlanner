e5e85c7ac731dd3cbf22273d8c6a60d6951063fb
Orden14 <t.lesrel@gmail.com>
2024-05-09 21:11:23 +0200

    Codeception integration

---
+++ tests/Support/FunctionalTester.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Support;
+
+use App\Entity\User;
+use Codeception\Actor;
+
+/**
+ * Inherited Methods
+ *
+ * @method void wantTo($text)
+ * @method void wantToTest($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method void pause($vars = [])
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+final class FunctionalTester extends Actor
+{
+    use _generated\FunctionalTesterActions;
+
+    public function loginAs(string $username): User
+    {
+        $user = $this->grabEntityFromRepository(User::class, ['username' => $username]);
+        $this->amLoggedInAs($user);
+
+        return $user;
+    }
+}
