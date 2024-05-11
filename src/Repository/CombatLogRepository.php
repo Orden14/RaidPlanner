522343c59c19625ff76736a1e28561e12dcabc86
Orden14 <t.lesrel@gmail.com>
2024-05-11 18:45:39 +0200

    update combatlog repo

--- src/Repository/CombatLogRepository.php
+++ src/Repository/CombatLogRepository.php
@@ -8,36 +8,16 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<CombatLog>
+ *
+ * @method CombatLog|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CombatLog|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CombatLog[]    findAll()
+ * @method CombatLog[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class CombatLogRepository extends ServiceEntityRepository
+final class CombatLogRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, CombatLog::class);
     }
-
-//    /**
-//     * @return CombatLog[] Returns an array of CombatLog objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?CombatLog
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
