eee69733bf1b4ecd407e6e5a0b189117808fc2ea
Orden14 <t.lesrel@gmail.com>
2024-05-15 20:38:58 +0200

    update security

--- src/Checker/EventParticipationPermission/Steps/CheckUserIsAllowedStep.php
+++ src/Checker/EventParticipationPermission/Steps/CheckUserIsAllowedStep.php
@@ -3,6 +3,7 @@
 namespace App\Checker\EventParticipationPermission\Steps;
 
 use App\Entity\GuildEvent;
+use App\Entity\User;
 use App\Enum\RolesEnum;
 use App\Interface\EventParticipationPermissionStepInterface;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -15,7 +16,11 @@ final readonly class CheckUserIsAllowedStep implements EventParticipationPermiss
 
     public function check(GuildEvent $guildEvent): bool
     {
+        /** @var User $currentUser */
+        $currentUser = $this->security->getUser();
+
         return $this->security->isGranted(RolesEnum::TRIAL->value)
+            || $guildEvent->getOwner() === $currentUser
             || ($guildEvent->isOldMembersAllowed() && $this->security->isGranted(RolesEnum::OLD_MEMBER->value));
     }
 }
