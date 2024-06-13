d5873d660cc201ffd554b02ae51c97675384f6af
Orden14 <t.lesrel@gmail.com>
2024-06-13 22:29:28 +0200

    security update

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -5,8 +5,10 @@ namespace App\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -22,12 +24,19 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
 
     public const string LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private readonly UrlGeneratorInterface $urlGenerator) {}
+    public function __construct(
+        private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly RateLimiterFactory    $authenticationRequestLimiter
+    ) {}
 
     final public function authenticate(Request $request): Passport
     {
-        $username = $request->getPayload()->getString('username');
+        $limiter = $this->authenticationRequestLimiter->create($request->getClientIp());
+        if (!$limiter->consume()->isAccepted()) {
+            throw new TooManyLoginAttemptsAuthenticationException();
+        }
 
+        $username = $request->getPayload()->getString('username');
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $username);
 
         return new Passport(
