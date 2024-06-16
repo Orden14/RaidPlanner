bd9788e3539db30ac39af41c0274e0ffd960e405
Orden <92043415+Orden14@users.noreply.github.com>
2024-06-16 22:49:18 +0200

    Registration upgrade (#27)

---
+++ assets/controllers/registration/password_requirements_controller.js
@@ -0,0 +1,124 @@
+import {Controller} from 'stimulus'
+
+export default class extends Controller {
+    connect() {
+        this.passwordRequirements()
+    }
+
+    passwordRequirements() {
+        let options = {
+            numCharacters: 10,
+            useLowercase: true,
+            useUppercase: true,
+            useNumbers: true,
+            infoMessage: '',
+            fadeTime:300
+        }
+
+        options.infoMessage = 'N\'utilisez pas votre mot de passe Guild Wars 2.'
+
+        let numCharactersUI = '<li class="pr-numCharacters"><span></span>Minimum 10 caractères</li>',
+            useLowercaseUI = '',
+            useUppercaseUI = '',
+            useNumbersUI   = ''
+
+        if (options.useLowercase === true) {
+            useLowercaseUI = '<li class="pr-useLowercase"><span></span>1 Minuscule</li>'
+        }
+        if (options.useUppercase === true) {
+            useUppercaseUI = '<li class="pr-useUppercase"><span></span>1 Majuscule</li>'
+        }
+        if (options.useNumbers === true) {
+            useNumbersUI = '<li class="pr-useNumbers"><span></span>1 Chiffre</li>'
+        }
+
+        let messageDiv = '<div id="pr-box"><i></i><div id="pr-box-inner"><p>' + options.infoMessage + '</p><ul>' + numCharactersUI + useLowercaseUI + useUppercaseUI + useNumbersUI + '</ul></div></div>'
+
+        let numCharactersDone = true,
+            useLowercaseDone = true,
+            useUppercaseDone = true,
+            useNumbersDone   = true
+
+        let showMessage = () => {
+            const prBoxElement = $("#pr-box")
+
+            if (numCharactersDone === false || useLowercaseDone === false || useUppercaseDone === false || useNumbersDone === false) {
+                if (prBoxElement.length === 0) {
+                    $(this.element).parent().append(messageDiv)
+                }
+                prBoxElement.fadeIn(options.fadeTime)
+            }
+        }
+
+        let deleteMessage = () => {
+            let targetMessage = $("#pr-box")
+            targetMessage.fadeOut(options.fadeTime, function(){
+                $(this).remove()
+            })
+        }
+
+        let checkCompleted = () => {
+            if (numCharactersDone === true && useLowercaseDone === true && useUppercaseDone === true && useNumbersDone === true) {
+                deleteMessage()
+            } else {
+                showMessage()
+            }
+        }
+
+        $(this.element).on("focus", () => {
+            showMessage()
+        })
+
+        $(this.element).on("blur", () => {
+            deleteMessage()
+        })
+
+        let lowerCase = /[a-z]/,
+            upperCase = /[A-Z]/,
+            numbers = /\d/
+
+        $(this.element).on("keyup focus", function (){
+            let thisVal = $(this).val()
+
+            checkCompleted()
+
+            if ( thisVal.length >= options.numCharacters ) {
+                $(".pr-numCharacters span").addClass("pr-ok")
+                numCharactersDone = true
+            } else {
+                $(".pr-numCharacters span").removeClass("pr-ok")
+                numCharactersDone = false
+            }
+
+            if (options.useLowercase === true) {
+                if ( thisVal.match(lowerCase) ) {
+                    $(".pr-useLowercase span").addClass("pr-ok")
+                    useLowercaseDone = true
+                } else {
+                    $(".pr-useLowercase span").removeClass("pr-ok")
+                    useLowercaseDone = false
+                }
+            }
+
+            if (options.useUppercase === true) {
+                if ( thisVal.match(upperCase) ) {
+                    $(".pr-useUppercase span").addClass("pr-ok")
+                    useUppercaseDone = true
+                } else {
+                    $(".pr-useUppercase span").removeClass("pr-ok")
+                    useUppercaseDone = false
+                }
+            }
+
+            if (options.useNumbers === true) {
+                if ( thisVal.match(numbers) ) {
+                    $(".pr-useNumbers span").addClass("pr-ok")
+                    useNumbersDone = true
+                } else {
+                    $(".pr-useNumbers span").removeClass("pr-ok")
+                    useNumbersDone = false
+                }
+            }
+        })
+    }
+}
--- src/Form/Security/RegistrationType.php
+++ src/Form/Security/RegistrationType.php
@@ -5,6 +5,7 @@ namespace App\Form\Security;
 use App\Entity\User;
 use App\Util\Security\PasswordConstraintUtil;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -31,6 +32,10 @@ final class RegistrationType extends AbstractType
             ->add('registrationToken', TextType::class, [
                 'mapped' => false,
                 'required' => true,
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'required' => true
             ]);
     }
 
