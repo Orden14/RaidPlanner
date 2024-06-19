9a152503851ded7afcc520b2fe38846550121366
Orden14 <t.lesrel@gmail.com>
2024-06-19 14:18:19 +0200

    trim dependencies 1

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -4,8 +4,6 @@ import $ from 'jquery'
 global.$ = $
 global.jQuery = $
 global.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
-require('jquery-confirm')
-require('jquery-confirm/css/jquery-confirm.css')
 
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
