c8cbb14575dfe661bdcaa168c7c6a18953e8d0ad
Orden14 <t.lesrel@gmail.com>
2024-06-20 17:14:38 +0200

    test

--- src/Checker/EventManagementPermission/EventManagementPermissionChecker.php
+++ src/Checker/EventManagementPermission/EventManagementPermissionChecker.php
@@ -7,14 +7,13 @@ use App\Interface\EventManagementPermissionStepInterface;
 use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
 
 final readonly class EventManagementPermissionChecker
-
-
 {
-    public function __construct (
+    public function __construct(
         /** @var EventManagementPermissionStepInterface[] $eventManagementPermissionStepInterfaces */
         #[TaggedIterator(EventManagementPermissionStepInterface::class)]
         private iterable $eventManagementPermissionStepInterfaces
-    ) {}
+    ) {
+    }
 
     public function checkIfUserCanManageEvent(GuildEvent $guildEvent): bool
     {
